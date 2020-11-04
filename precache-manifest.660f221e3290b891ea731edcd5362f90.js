34180e4580590fec6f2875522e36d879a9ed2e0e
migasubi <yuri.ossipov@gmail.com>
2020-11-03 23:12:00 -0500

    Updates

---
+++ precache-manifest.660f221e3290b891ea731edcd5362f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e14205fcd4f85c82a3dd3364fc19a40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08563e2f76502397023c",
+    "url": "/monsters-rolodex/static/css/main.469e42ac.chunk.css"
+  },
+  {
+    "revision": "56aae774d8f9eeb2cd6c",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08563e2f76502397023c",
+    "url": "/monsters-rolodex/static/js/main.1806bce9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
